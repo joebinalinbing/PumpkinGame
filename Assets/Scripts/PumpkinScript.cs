24597a360e73934c9bd068d024ea170ac586f7c0
Joe Binalinbing <40000544+joebinalinbing@users.noreply.github.com>
2019-03-07 23:40:19 -0800

    PUmpkin game
    ya

---
+++ Assets/Scripts/PumpkinScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpkinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (transform.position.y < -3f) {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter(Collision target)
+    {
+        if(target.gameObject.tag== "Golem")
+        {
+
+
+            Destroy(target.gameObject);
+            Time.timeScale = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Transform[] spawnSpoints;
+    public GameObject pumpkin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    IEnumerator StartSpawning() {
+        yield return new WaitForSeconds(Random.Range(1f, 3.5f));
+
+        Instantiate(pumpkin, spawnSpoints[Random.Range(0, spawnSpoints.Length)].position, Quaternion.identity);
+
+        StartCoroutine(StartSpawning());
+    }
+    
+
+
+
+}
