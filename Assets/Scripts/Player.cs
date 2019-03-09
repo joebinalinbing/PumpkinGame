c837b1e3d7b876c704ff277c798229d03cdefb38
Joe Binalinbing <40000544+joebinalinbing@users.noreply.github.com>
2019-03-08 18:27:34 -0800

    g
    g

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    public float health = 100f;
+    public string name = "Wizard";
+    public void PlayerInfo()
+    {
+
+        Debug.Log("Player Name is: " + name + "Players Health is: " + health);
+    }
+
+}
---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    float power = 2.4f;
+    double health = 3.5;
+    int score = 44;
+    string name = "Wizards";
+    bool isAlive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player wizard = new Player();
+        wizard.PlayerInfo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
